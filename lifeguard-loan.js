ae176b85860b52bb45918df1bcb03376b10ff3bf
juvilleg <juvilleg@bancolombia.com.co>
2022-11-22 12:39:25 -0500

    add lifeguard-loan.js and refactor

--- lifeguard-loan.js
+++ lifeguard-loan.js
@@ -1,16 +1,12 @@
 import {convertToDecimal, convertToPercentage, roundDecimals, onlyNumberKey, cleanMask, maskValue, handleKeyUpThousandSeparators} from './shared/utils.js'
 
 const interestRateEA = 26.08; // Valor dado en %
-const commissionFGA = 7.5; // Valor dado en %
 const sure = 1450; // Valor dado en pesos ($) x cada millón
-const iva = 19; // Valor dado en %
-const initValue = 1000000; // Valor prestamo inicial (Para que cargue una simulacion por defecto)
+const initValue = 500000; // Valor prestamo inicial (Para que cargue una simulacion por defecto)
 
 const interestRateMV = calculateInterestRateMV(interestRateEA);
-const commissionFgaIva = calculateCommissionFgaIva(commissionFGA, iva);
 let loanValue = null, 
-loanValueCommission = null, 
-numberInstallments = null, 
+numberInstallments = 1, 
 sureCalculated = null, 
 feeValue = null;
 
@@ -39,38 +35,30 @@ function handleClickCalculate() {
 
 function calculate() {
     loanValue = cleanMask(document.getElementById('input-value').value);
-    numberInstallments = document.getElementById('months').value;
 
-    if(!loanValue || !numberInstallments) return;
+    if(!loanValue) return;
 
-    loanValueCommission = calculateLoanValueCommission();
     sureCalculated = calculateSure();
     feeValue = calculateFeeValue();
 }
 
 function printInfo() {
     document.getElementById('loan-value').innerHTML = `$ ${maskValue(loanValue)}`;
-    document.getElementById('loan-value-commission').innerHTML = `$ ${maskValue(loanValueCommission)}`;
     document.getElementById('fee-value').innerHTML = `$ ${maskValue(feeValue)}`;
     document.getElementById('interest').innerHTML = `${interestRateMV} % E.M (${interestRateEA}% E.A)`;
-    document.getElementById('number-installments').innerHTML = `${numberInstallments} meses`;
+    document.getElementById('number-installments').innerHTML = `${numberInstallments} mes`;
     document.getElementById('sure').innerHTML = `$ ${maskValue(sureCalculated)}`;
 }
 
 /* --------- Loan Calculations ------------ */
-function calculateLoanValueCommission() {
-    const loanValueCommission = loanValue * ( 1 + convertToDecimal(commissionFgaIva) );;
-    return Math.round(loanValueCommission);
-}
-
 function calculateSure() {
-    let numberSures = Math.ceil( loanValueCommission / 1000000 );
+    let numberSures = Math.ceil( loanValue / 1000000 );
     return sure * numberSures;
 }
 
 function calculateFeeValue() {
     const interestRateMVDecimal = convertToDecimal(interestRateMV);
-    const feeValueWithoutSure = (loanValueCommission / ((1 - (1 + interestRateMVDecimal) ** (numberInstallments * -1) ) / interestRateMVDecimal));
+    const feeValueWithoutSure = (loanValue / ((1 - (1 + interestRateMVDecimal) ** (numberInstallments * -1) ) / interestRateMVDecimal));
     const feeValueWithSure = feeValueWithoutSure + sureCalculated;
     return Math.round(feeValueWithSure);
 }
@@ -82,14 +70,6 @@ function calculateInterestRateMV (interestRateEA) {
     interestRateMV = convertToPercentage(interestRateMV);
     return roundDecimals(interestRateMV);
 }
-
-function calculateCommissionFgaIva(commissionFGA, iva) {
-    const commissionFGADecimal = convertToDecimal(commissionFGA);
-    const ivaDecimal = convertToDecimal(iva);
-    let commissionFgaIva = commissionFGADecimal * (1 + ivaDecimal);
-    commissionFgaIva = convertToPercentage(commissionFgaIva);
-    return roundDecimals(commissionFgaIva);
-} 
 
 inputValue.onkeyup = handleKeyUpThousandSeparators;
 inputValue.onkeypress = onlyNumberKey;
---
+++ shared/utils.js
@@ -0,0 +1,34 @@
+export function convertToDecimal(value) {
+    return value / 100;
+}
+
+export function convertToPercentage(value) {
+    return value * 100;
+}
+
+export function roundDecimals(value) {
+    return Math.ceil(value * 100) / 100;
+}
+
+export function onlyNumberKey(event) {
+    const ASCIICode = (event.which) ? event.which : event.keyCode
+    if (ASCIICode > 31 && (ASCIICode < 48 || ASCIICode > 57))
+        return false;
+    return true;
+}
+
+export function cleanMask(value) {
+    if(!value) return '';
+    return value.replace(/\./gi, '');
+}
+
+export function maskValue(value) {
+    if(!value) return '';
+    return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.');
+}
+
+export function handleKeyUpThousandSeparators(event) {
+    var tempNumber = cleanMask(event.target.value);
+    var commaSeparatedNumber = tempNumber.split(/(?=(?:\d{3})+$)/).join('.');
+    event.target.value = commaSeparatedNumber;
+}
