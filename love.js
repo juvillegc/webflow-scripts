8b3fe2375f2daddb580ac2e10883219e1193df2f
edhurtad <120751523+edhurtad@users.noreply.github.com>
2023-09-13 15:24:47 -0500

    Update love.js

--- love.js
+++ love.js
@@ -1,15 +1,18 @@
 import { validPhoneNumber, validateEmail } from './shared/utils.js';
 
 
-const inputPhoneNumber = document.getElementById('.numero_celular_love');
+const inputPhoneNumber = document.querySelectorAll('.numero_celular_love');
 
 
 
 const validateInputs = () => {
     
-    inputPhoneNumber.onkeypress = validPhoneNumber;
+     inputPhoneNumber.forEach((input) => {
+        input.onkeypress = inputPhoneNumber;
+    });
     
 }
+ 
 
 
 const main = async () => {
