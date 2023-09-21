d486cf7a1078f5bb2c48a61d3d977b33ba6166f5
edhurtad <120751523+edhurtad@users.noreply.github.com>
2023-09-21 10:03:48 -0500

    Update gamer.js

--- gamer.js
+++ gamer.js
@@ -1,1 +1,21 @@
+import { validPhoneNumber, validateEmail } from './shared/utils.js';
 
+
+const inputPhoneNumber = document.getElementById('numero_celular');
+const inputDocumentMail = document.getElementById('correo_electronico');
+
+
+const validateInputs = () => {
+    
+    inputPhoneNumber.onkeypress = validPhoneNumber;
+    
+    inputDocumentMail.oninput = validateEmail;
+    
+}
+
+
+const main = async () => {
+    validateInputs();
+}
+
+main();
