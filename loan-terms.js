e85b18d940190c268135cf0acee4ecf640d792d1
edhurtad <120751523+edhurtad@users.noreply.github.com>
2023-11-17 14:26:20 -0500

    Update loan-terms.js
    codigo para validar inputs

--- loan-terms.js
+++ loan-terms.js
@@ -1,1 +1,24 @@
+import { validPhoneNumber, validateEmail, validDocumentNumber } from './shared/utils.js';
 
+
+const inputPhoneNumber = document.getElementById('numero_celular');
+const inputDocumentMail = document.getElementById('correo_electronico');
+const inputDocumentNumber = document.getElementById('numero_documento');
+
+
+const validateInputs = () => {
+    
+    inputPhoneNumber.onkeypress = validPhoneNumber;
+    
+    inputDocumentMail.oninput = validateEmail;
+
+    inputDocumentNumber.onkeypress = validDocumentNumber;
+    
+}
+
+
+const main = async () => {
+    validateInputs();
+}
+
+main();
