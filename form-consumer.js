0b158f529b712d21b32c7c8e67ce1c0e1f740583
edhurtad <120751523+edhurtad@users.noreply.github.com>
2024-04-16 09:22:43 -0500

    Update form-consumer.js
    colocar departamento y ciudad

--- form-consumer.js
+++ form-consumer.js
@@ -1,11 +1,14 @@
 
-import { validPhoneNumber, validateEmail, validDocumentNumber, handleKeyUpThousandSeparators, onlyNumberKey} from './shared/utils.js';
+import { validPhoneNumber, validateEmail, validDocumentNumber, handleKeyUpThousandSeparators, onlyNumberKey, removeAllOptions, addFirstOption} from './shared/utils.js';
 import { inputEvent } from './shared/date-format.js';
+import { getDepartments, getCities } from './services/location.service.js';
 
 
 const inputDocumentMail = document.getElementById('correo_electronico');
 const inputDocumentNumber = document.getElementById('numero_documento');
 const inputDateTakeMoney = document.getElementById('fecha_recarga');
+const selDepartments = document.querySelector('#departamentos');
+const selCities = document.querySelector('#ciudades');
 const inputPhoneNumber = document.querySelectorAll('.numero_cedula_gamer');
 const customPrice = document.querySelectorAll('.custom-price');
 
@@ -27,6 +30,30 @@ const validateInputs = () => {
 
     inputDateTakeMoney.oninput = inputEvent;
     
+}
+
+const loadCities = async (keyDepartment) => {
+    removeAllOptions(selCities);
+    addFirstOption('Seleccione la ciudad', selCities);
+    const cities = await getCities(keyDepartment);
+    cities.forEach(city => {
+        const option = document.createElement('option');
+        option.value = city.id;
+        option.innerHTML = city.label;
+        selCities.appendChild(option);
+    });
+}
+
+const handleChangeDepartment = async () => {
+    const deparmentSelected = selDepartments.options[selDepartments.selectedIndex].getAttribute('key');
+    await loadCities(deparmentSelected);
+}
+
+const main = async () => {
+    validateInputs();
+    await loadDepartments();
+    addFirstOption('Seleccione la ciudad', selCities);
+    selDepartments.addEventListener('change', handleChangeDepartment);
 }
 
 
