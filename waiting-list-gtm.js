5bad7e54eb1a02634a40cc6319fd714f0f104685
edhurtad <120751523+edhurtad@users.noreply.github.com>
2024-05-09 11:38:15 -0500

    Update waiting-list-gtm.js

--- waiting-list-gtm.js
+++ waiting-list-gtm.js
@@ -1,16 +1,21 @@
-import { validPhoneNumber,  validDocumentNumber } from './shared/utils.js';
+import { validPhoneNumber,  validDocumentNumber, validateEmail } from './shared/utils.js';
+import { inputEvent } from './shared/date-format.js';
 
 
 const inputPhoneNumber = document.getElementById('numero_celular');
-
+const inputDateTakeMoney = document.getElementById('fecha_nacimiento');
 const inputDocumentNumber = document.getElementById('numero_documento');
+const inputDocumentMail = document.getElementById('correo_electronico');
+
 
 
 const validateInputs = () => {
     
-    inputPhoneNumber.onkeypress = validPhoneNumber;
-    
     inputDocumentNumber.onkeypress = validDocumentNumber;
+    inputDateTakeMoney.oninput = inputEvent;
+    inputDocumentMail.oninput = validateEmail;
+    inputPhoneNumber.onkeypress = validPhoneNumber;
+  
     
 }
 
