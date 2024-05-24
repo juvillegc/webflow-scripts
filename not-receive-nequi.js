76805729d3d25daf931ce8a768938677eaf2d101
edhurtad <120751523+edhurtad@users.noreply.github.com>
2024-05-24 14:01:58 -0500

    Update not-receive-nequi.js

--- not-receive-nequi.js
+++ not-receive-nequi.js
@@ -6,53 +6,59 @@ const selDepartments = document.querySelectorAll('.departamentos');
 const selCities = document.querySelectorAll('.ciudades');
 const inputPhoneNumber = document.querySelectorAll('.numero_celular');
 
-
-
 const validateInputs = () => {
-    
-     inputPhoneNumber.forEach((input) => {
+    inputPhoneNumber.forEach((input) => {
         input.onkeypress = validPhoneNumber;
     });
-    
 }
 
 const loadDepartments = async () => {
     const { deparments } = await getDepartments();
 
-    addFirstOption('Seleccione el departamento', selDepartments);
-    deparments.forEach(department => {
-        const option = document.createElement('option');
-        option.value = department.id;
-        option.setAttribute('key', department.key);
-        option.innerHTML = department.label;
-        selDepartments.appendChild(option);
+    selDepartments.forEach((selDepartment) => {
+        addFirstOption('Seleccione el departamento', selDepartment);
+        deparments.forEach(department => {
+            const option = document.createElement('option');
+            option.value = department.id;
+            option.setAttribute('key', department.key);
+            option.innerHTML = department.label;
+            selDepartment.appendChild(option);
+        });
     });
 }
 
-
 const loadCities = async (keyDepartment) => {
-    removeAllOptions(selCities);
-    addFirstOption('Seleccione la ciudad', selCities);
+    selCities.forEach((selCity) => {
+        removeAllOptions(selCity);
+        addFirstOption('Seleccione la ciudad', selCity);
+    });
+
     const cities = await getCities(keyDepartment);
     cities.forEach(city => {
         const option = document.createElement('option');
         option.value = city.id;
         option.innerHTML = city.label;
-        selCities.appendChild(option);
+        selCities.forEach((selCity) => {
+            selCity.appendChild(option.cloneNode(true));
+        });
     });
 }
 
-const handleChangeDepartment = async () => {
-    const deparmentSelected = selDepartments.options[selDepartments.selectedIndex].getAttribute('key');
-    await loadCities(deparmentSelected);
+const handleChangeDepartment = async (event) => {
+    const selDepartment = event.target;
+    const keyDepartment = selDepartment.options[selDepartment.selectedIndex].getAttribute('key');
+    await loadCities(keyDepartment);
 }
 
 const main = async () => {
     validateInputs();
     await loadDepartments();
-    addFirstOption('Seleccione la ciudad', selCities);
-    selDepartments.addEventListener('change', handleChangeDepartment);
+    selCities.forEach((selCity) => {
+        addFirstOption('Seleccione la ciudad', selCity);
+    });
+    selDepartments.forEach((selDepartment) => {
+        selDepartment.addEventListener('change', handleChangeDepartment);
+    });
 }
-
 
 main();
