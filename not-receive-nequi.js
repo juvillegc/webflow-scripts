415a3f1af4b777103679dc2d43a305ddb35aab30
edhurtad <120751523+edhurtad@users.noreply.github.com>
2024-05-24 11:44:15 -0500

    Update not-receive-nequi.js

--- not-receive-nequi.js
+++ not-receive-nequi.js
@@ -1,31 +1,27 @@
-
 import { validPhoneNumber, handleKeyUpThousandSeparators, onlyNumberKey, removeAllOptions, addFirstOption } from './shared/utils.js';
 import { getDepartments, getCities } from './services/location.service.js';
-
 
 const selDepartments = document.querySelectorAll('.departamentos'); 
 const selCities = document.querySelectorAll('.ciudades');
 const inputPhoneNumber = document.querySelectorAll('.numero_celular');
 
-
-
 const validateInputs = () => {
-    
-     inputPhoneNumber.forEach((input) => {
-        input.onkeypress = validPhoneNumber;
+    inputPhoneNumber.forEach((input) => {
+        input.addEventListener('keypress', validPhoneNumber);
     });
-    
 }
 
 const loadDepartments = async () => {
-    const { deparments } = await getDepartments();
+    const { departments } = await getDepartments(); // Corrección en el nombre de la variable
     addFirstOption('Seleccione el departamento', selDepartments);
-    deparments.forEach(department => {
+    departments.forEach(department => {
         const option = document.createElement('option');
         option.value = department.id;
         option.setAttribute('key', department.key);
         option.innerHTML = department.label;
-        selDepartments.appendChild(option);
+        selDepartments.forEach(select => {
+            select.appendChild(option); // Corrección aquí, usar forEach para agregar a todos los select
+        });
     });
 }
 
@@ -37,21 +33,23 @@ const loadCities = async (keyDepartment) => {
         const option = document.createElement('option');
         option.value = city.id;
         option.innerHTML = city.label;
-        selCities.appendChild(option);
+        selCities.forEach(select => {
+            select.appendChild(option); // Corrección aquí, usar forEach para agregar a todos los select
+        });
     });
 }
 
 const handleChangeDepartment = async () => {
-    const deparmentSelected = selDepartments.options[selDepartments.selectedIndex].getAttribute('key');
-    await loadCities(deparmentSelected);
+    const departmentSelected = selDepartments.options[selDepartments.selectedIndex].getAttribute('key'); // Corrección en el nombre de la variable
+    await loadCities(departmentSelected);
 }
 
 const main = async () => {
     validateInputs();
     await loadDepartments();
-    addFirstOption('Seleccione la ciudad', selCities);
-    selDepartments.addEventListener('change', handleChangeDepartment);
+    selDepartments.forEach(select => {
+        select.addEventListener('change', handleChangeDepartment); // Corrección aquí, usar forEach para agregar el listener a todos los select
+    });
 }
-
 
 main();
