be3e043148685661aced87a265abfb1c2d58763b
edhurtad <120751523+edhurtad@users.noreply.github.com>
2024-09-30 16:54:06 -0500

    Update propellant-loan.js
    cambio de tasa

--- propellant-loan.js
+++ propellant-loan.js
@@ -1,6 +1,6 @@
 import {convertToDecimal, convertToPercentage, round, roundDecimals, onlyNumberKey, cleanMask, maskValue, handleKeyUpThousandSeparators} from './shared/utils.js'
 
-const interestRateEA = 28.55; // Valor dado en %
+const interestRateEA = 27.87; // Valor dado en %
 const commissionFGA = 10; // Valor dado en %
 const sure = 1450; // Valor dado en pesos ($) x cada millón
 const iva = 19; // Valor dado en %
