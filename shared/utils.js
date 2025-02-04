765d978323138767a5d100562ac935f4d353925b
edhurtad <120751523+edhurtad@users.noreply.github.com>
2025-02-04 08:50:41 -0500

    Update utils.js

--- shared/utils.js
+++ shared/utils.js
@@ -30,7 +30,7 @@ export function limitCharacters(event, limit) {
 export function validPhoneNumber(event) {
     if (!onlyNumberKey(event)) return false;
     if (!limitCharacters(event, 10)) return false;
-      event.target.value = event.target.value.replace(/\s/g, "");
+      
     setTimeout(() => {
         event.target.value = event.target.value.replace(/\s/g, "");
     }, 0);
