23b98e92b5714eb043c1755bce55889ed37482b7
edhurtad <120751523+edhurtad@users.noreply.github.com>
2025-02-14 08:43:42 -0500

    Update utils.js

--- shared/utils.js
+++ shared/utils.js
@@ -95,3 +95,11 @@ export const addFirstOption = (label, select) => {
     option.innerHTML = label;
     select.appendChild(option);
 }
+
+
+export const normalizeTex = (str) => {
+    return str.normalize('NFD')
+    .replace(/[\u0300-\u036f]/g, '')
+    .replace(/ñ/g, 'n')
+    .replace(/Ñ/g, 'N'); 
+}
