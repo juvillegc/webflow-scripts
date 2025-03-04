98676ed8dacbbc4d50399f36f59f25a361a4cfba
edhurtad <120751523+edhurtad@users.noreply.github.com>
2025-03-04 07:44:30 -0500

    Update ecosystem.js

--- ecosystem.js
+++ ecosystem.js
@@ -1,1 +1,22 @@
 
+import { validPhoneNumber, validateEmail } from './shared/utils.js';
+
+
+const inputPhoneNumber = document.getElementById('numero_celular');
+const inputDocumentMail = document.getElementById('correo_electronico');
+
+
+const validateInputs = () => {
+    
+    inputPhoneNumber.onkeypress = validPhoneNumber;
+    
+    inputDocumentMail.oninput = validateEmail;
+    
+}
+
+
+const main = async () => {
+    validateInputs();
+}
+
+main();
