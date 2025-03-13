3c327035da07168ccb928baf2d88713999c4f101
edhurtad <120751523+edhurtad@users.noreply.github.com>
2025-03-13 14:20:49 -0500

    Update event.clevertap.js

--- services/event.clevertap.js
+++ services/event.clevertap.js
@@ -1,1 +1,38 @@
 
+document.addEventListener("DOMContentLoaded", function() {
+    const formClevertap = document.querySelector('#wf-form-clevertap');
+    if (formClevertap) {
+        formClevertap.addEventListener('submit', function(event) {
+            event.preventDefault(); // Evita el envío inmediato del formulario
+            
+            const name = formClevertap.querySelector("input[name='userName']").value;
+            const email = formClevertap.querySelector("input[name='userEmail']").value;
+            let phone = formClevertap.querySelector("input[name='userPhone']").value;
+            
+            // Asegúrate de que el número de teléfono tenga el formato internacional
+            if (!phone.startsWith("+")) {
+                phone = "+57" + phone; // Reemplaza "+57" con tu código de país
+            }
+            
+            // Identificamos o actualizamos el usuario
+            clevertap.onUserLogin.push({
+                "Site": {
+                    "Name": name,
+                    "Email": email,
+                    "Phone": phone
+                }
+            });
+            
+            // Disparamos el evento personalizado
+            clevertap.event.push("Webflow-subscribe-click", {
+                "Name": name,
+                "Email": email,
+                "Phone": phone
+            });
+            
+            console.log("Usuario logueado y evento 'Webflow-subscribe-click' enviado a CleverTap.");
+        });
+    }
+});
+
+
