473bc269ea5cd01f5e3b9a9f1567f366f3070bd0
edhurtad <120751523+edhurtad@users.noreply.github.com>
2025-03-19 14:49:01 -0500

    Update event.clevertap.js

--- services/event.clevertap.js
+++ services/event.clevertap.js
@@ -1,28 +1,49 @@
 
-export function sendCleverTapEvent(phone, loanValue, months) {
-  if (!phone || !loanValue || !months) {
-    console.warn("Faltan parámetros para enviar el evento a CleverTap");
-    return;
-  }
+export const sendCleverTapEvent = (eventName, properties = {}) => {
 
-  if (!phone.startsWith("+")) {
-    phone = "+57" + phone; 
-  }
-
-  clevertap.onUserLogin.push({
-    "Site": {
-      "Phone": phone,
-      "LoanValue": loanValue,
-      "Months": months
+    if(!eventName || typeof properties !== 'object' || Object.keys(properties).length === 0){
+        console.warn("Parámetros inválidos para enviar el evento a CleverTap");
+        return;
     }
-  });
 
-  clevertap.event.push("SimuladorDatos", {
-    "Phone": phone,
-    "LoanValue": loanValue,
-    "Months": months
-  });
+    if (properties.Phone && typeof properties.Phone === 'string' && !properties.Phone.startsWith("+")){
+        properties.Phone = "+57" + properties.Phone;
+    
+    }
+
+    clevertap.onUserLogin.push({
+        Site: properties
+      });
+      
+     
+      clevertap.event.push(eventName, properties);
+    
 }
 
 
-
+// export function sendCleverTapEvent(phone, loanValue, months) {
+//     if (!phone || !loanValue || !months) {
+//       console.warn("Faltan parámetros para enviar el evento a CleverTap");
+//       return;
+//     }
+  
+//     if (!phone.startsWith("+")) {
+//       phone = "+57" + phone; 
+//     }
+  
+//     clevertap.onUserLogin.push({
+//       'Site': {
+//         'Phone': phone,
+//         'LoanValue': loanValue,
+//         'Months': months
+//       }
+//     });
+  
+//     clevertap.event.push("SimuladorDatos", {
+//       'Phone': phone,
+//       'LoanValue': loanValue,
+//       'Months': months
+//     });
+//   }
+  
+  
