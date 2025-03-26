77fa560784fcc5ddb05f49ad9c05d0121848a0f2
edhurtad <120751523+edhurtad@users.noreply.github.com>
2025-03-26 14:06:29 -0500

    Update tech-bussines.js

--- tech-bussines.js
+++ tech-bussines.js
@@ -1,4 +1,4 @@
-import { validPhoneNumber, validateEmail, validDocumentNumber, onlyNumberKey } from './shared/utils.js';
+import { validPhoneNumber, validateEmail, validDocumentNumber } from './shared/utils.js';
 
 const inputPhoneNumber = document.getElementById('numero_celular');
 const inputDocumentMail = document.getElementById('correo_electronico');
