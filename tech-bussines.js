50a6a307b5c5dbba13b74996c42da876b7165c80
edhurtad <120751523+edhurtad@users.noreply.github.com>
2025-03-26 13:47:54 -0500

    Update tech-bussines.js

--- tech-bussines.js
+++ tech-bussines.js
@@ -4,34 +4,24 @@ const inputPhoneNumber = document.getElementById('numero_celular');
 const inputDocumentMail = document.getElementById('correo_electronico');
 const inputDocumentNumber = document.querySelectorAll('.numero_documento');
 
+
+
 const validateInputs = () => {
-    // Validar campo número de celular
-    if (inputPhoneNumber) {
-        inputPhoneNumber.addEventListener('paste', (e) => e.preventDefault());
-        inputPhoneNumber.addEventListener('copy', (e) => e.preventDefault());
-        inputPhoneNumber.addEventListener('keypress', validPhoneNumber);
-    }
+    
+   inputPhoneNumber.forEach((input) => {
+        input.onkeypress = validPhoneNumber;
+    });
+    
+    inputDocumentMail.oninput = validateEmail;
 
-    // Validar campo de correo electrónico
-    if (inputDocumentMail) {
-        inputDocumentMail.addEventListener('paste', (e) => e.preventDefault());
-        inputDocumentMail.addEventListener('copy', (e) => e.preventDefault());
-        inputDocumentMail.addEventListener('input', validateEmail);
-    }
-
-    // Validar campos de número de documento
-    if (inputDocumentNumber.length > 0) {
-        inputDocumentNumber.forEach((input) => {
-            input.addEventListener('keypress', (e) => {
-                validDocumentNumber(e);
-                onlyNumberKey(e);
-            });
-        });
-    }
-};
+    inputDocumentNumber.onkeypress = validDocumentNumber;
+    
+}
 
 const main = async () => {
     validateInputs();
 };
 
 main();
+
+
