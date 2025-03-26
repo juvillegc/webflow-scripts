ceed8f8fafeb6a87385013592b85e62511729559
edhurtad <120751523+edhurtad@users.noreply.github.com>
2025-03-26 12:04:33 -0500

    Update tech-bussines.js

--- tech-bussines.js
+++ tech-bussines.js
@@ -1,25 +1,33 @@
-import { validPhoneNumber,  validateEmail, validDocumentNumber, onlyNumberKey } from './shared/utils.js';
+import { validPhoneNumber, validateEmail, validDocumentNumber, onlyNumberKey } from './shared/utils.js';
 
 const inputPhoneNumber = document.getElementById('numero_celular');
 const inputDocumentMail = document.getElementById('correo_electronico');
 const inputDocumentNumber = document.querySelectorAll('.numero_documento');
 
 const validateInputs = () => {
-    // Deshabilitar copiar y pegar en el campo de celular
-    inputPhoneNumber.onpaste = (e) => e.preventDefault();
-    inputPhoneNumber.oncopy = (e) => e.preventDefault();
-    inputPhoneNumber.onkeypress = validPhoneNumber;
-    
-    // Deshabilitar copiar y pegar en el campo de correo electrónico y validar
-    inputDocumentMail.onpaste = (e) => e.preventDefault();
-    inputDocumentMail.oncopy = (e) => e.preventDefault();
-    inputDocumentMail.oninput = validateEmail;
+    // Validar campo número de celular
+    if (inputPhoneNumber) {
+        inputPhoneNumber.addEventListener('paste', (e) => e.preventDefault());
+        inputPhoneNumber.addEventListener('copy', (e) => e.preventDefault());
+        inputPhoneNumber.addEventListener('keypress', validPhoneNumber);
+    }
 
-    inputDocumentNumber.onkeypress = validDocumentNumber;
+    // Validar campo de correo electrónico
+    if (inputDocumentMail) {
+        inputDocumentMail.addEventListener('paste', (e) => e.preventDefault());
+        inputDocumentMail.addEventListener('copy', (e) => e.preventDefault());
+        inputDocumentMail.addEventListener('input', validateEmail);
+    }
 
-      inputDocumentNumber.forEach((input) => {
-        input.onkeypress = onlyNumberKey;
-    });
+    // Validar campos de número de documento
+    if (inputDocumentNumber.length > 0) {
+        inputDocumentNumber.forEach((input) => {
+            input.addEventListener('keypress', (e) => {
+                validDocumentNumber(e);
+                onlyNumberKey(e);
+            });
+        });
+    }
 };
 
 const main = async () => {
