e9cfd1bf396a6cc361cb458572e5ef90ebdd962a
edhurtad <120751523+edhurtad@users.noreply.github.com>
2025-04-11 11:04:10 -0500

    Update consummer.js

--- consummer.js
+++ consummer.js
@@ -1,4 +1,4 @@
-import { onlyNumberKey, handleKeyUpThousandSeparators, validPhoneNumber, validateEmail, validDocumentNumber, removeAllOptions, addFirstOption } from './shared/utils.js';
+import { onlyNumberKey, handleKeyUpThousandSeparators, validPhoneNumber, validateEmail, validDocumentNumber, removeAllOptions, addFirstOption, setTodayAsDefaultDate } from './shared/utils.js';
 import { inputEvent } from './shared/date-format.js';
 import { getDepartments, getCities } from './services/location.service.js';
 
@@ -8,21 +8,38 @@ const inputPhoneNumber = document.getElementById('numero_celular');
 const selDepartments = document.querySelector('#departamentos');
 const selCities = document.querySelector('#ciudades');
 const customPrice = document.querySelectorAll('.custom-price');
+const inputDate = document.querySelector('input[type="date"]');
 
 
 const validateInputs = () => {
     
     inputPhoneNumber.onkeypress = validPhoneNumber;
-    
     customPrice.forEach((input) => {
         input.onkeyup = handleKeyUpThousandSeparators;
         input.onkeypress = onlyNumberKey;
     });
-
     inputDocumentMail.oninput = validateEmail;
     inputDocumentNumber.onkeypress = validDocumentNumber;
 
+    setTodayAsDefaultDate(inputDate);
+
+    fileInputs.forEach(input => {
+  const maxSize = parseInt(input.getAttribute("data-max-size")) || 5242880;
+  input.addEventListener("change", () => {
+    const file = input.files[0];
+    if (file && file.size > maxSize) {
+      input.setCustomValidity(`El archivo excede el tamaño máximo permitido de ${(maxSize / 1048576)} MB.`);
+      input.reportValidity();
+      input.value = "";
+    } else {
+      input.setCustomValidity("");
+    }
+  });
+});
+
 }
+
+
 
 
 
