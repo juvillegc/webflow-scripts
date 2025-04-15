f4c63621310d1e1ff7ea5606510b31a476041513
edhurtad <120751523+edhurtad@users.noreply.github.com>
2025-04-15 11:01:55 -0500

    Update consummer.js

--- consummer.js
+++ consummer.js
@@ -1,4 +1,4 @@
-import { onlyNumberKey, handleKeyUpThousandSeparators, validPhoneNumber, validateEmail, validDocumentNumber, removeAllOptions, addFirstOption, setTodayAsDefaultDate } from './shared/utils.js';
+import { onlyNumberKey, handleKeyUpThousandSeparators, validPhoneNumber, validateEmail, validDocumentNumber, removeAllOptions, addFirstOption, setTodayAsDefaultDate, setMaxFileSizeListeners } from './shared/utils.js';
 import { getDepartments, getCities } from './services/location.service.js';
 
 const inputDocumentNumber = document.getElementById('numero_documento');
@@ -11,35 +11,20 @@ const inputDate = document.querySelector('input[type="date"]');
 const fileInputs = document.querySelectorAll('input[type="file"]');
 
 const validateInputs = () => {
-    
-    inputPhoneNumber.onkeypress = validPhoneNumber;
-    customPrice.forEach((input) => {
-        input.onkeyup = handleKeyUpThousandSeparators;
-        input.onkeypress = onlyNumberKey;
-    });
-    inputDocumentMail.oninput = validateEmail;
-    inputDocumentNumber.onkeypress = validDocumentNumber;
+  inputPhoneNumber.onkeypress = validPhoneNumber;
 
-    setTodayAsDefaultDate(inputDate);
-
-    fileInputs.forEach(input => {
-  const maxSize = parseInt(input.getAttribute("data-max-size")) || 5242880;
-  input.addEventListener("change", () => {
-    const file = input.files[0];
-    if (file && file.size > maxSize) {
-      input.setCustomValidity(`El archivo excede el tamaño máximo permitido de ${(maxSize / 1048576)} MB.`);
-      input.reportValidity();
-      input.value = "";
-    } else {
-      input.setCustomValidity("");
-    }
+  customPrice.forEach((input) => {
+    input.onkeyup = handleKeyUpThousandSeparators;
+    input.onkeypress = onlyNumberKey;
   });
-});
 
-}
+  inputDocumentMail.oninput = validateEmail;
+  inputDocumentNumber.onkeypress = validDocumentNumber;
 
+  setTodayAsDefaultDate(inputDate);
 
-
+  setMaxFileSizeListeners(fileInputs);
+};
 
 
 const loadDepartments = async () => {
