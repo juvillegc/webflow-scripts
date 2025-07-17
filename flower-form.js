2c36518c94cddab06e97b0897235a4189594819d
edhurtad <120751523+edhurtad@users.noreply.github.com>
2025-07-17 11:49:10 -0500

    Update flower-form.js

--- flower-form.js
+++ flower-form.js
@@ -1,1 +1,23 @@
 
+import { validPhoneNumber, validDocumentNumber } from './shared/utils.js';
+
+
+const inputPhoneNumber = document.getElementById('numero_celular');
+const inputDocumentNumber = document.getElementById('numero_documento');
+
+
+const validateInputs = () => {
+    
+    inputPhoneNumber.onkeypress = validPhoneNumber;
+    
+
+    inputDocumentNumber.onkeypress = validDocumentNumber;
+    
+}
+
+
+const main = async () => {
+    validateInputs();
+}
+
+main();
