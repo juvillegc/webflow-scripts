2470fa8f193fc235d3c983c042853aa09aed7e95
edhurtad <120751523+edhurtad@users.noreply.github.com>
2025-07-31 15:20:03 -0500

    Update flower-form.js

--- flower-form.js
+++ flower-form.js
@@ -1,20 +1,21 @@
 import { validPhoneNumber, validateEmail } from './shared/utils.js';
 
-const inputEmail = document.getElementById('userEmail');
-const phoneInputs = document.querySelectorAll('.userPhone');
+const inputEmails   = document.querySelectorAll('#userEmail');
+const phoneInputs   = document.querySelectorAll('.userPhone');
 
 const validateInputs = () => {
+
   phoneInputs.forEach((input) => {
-    input.onpaste = (e) => e.preventDefault();
-    input.oncopy = (e) => e.preventDefault();
-    input.onkeypress = validPhoneNumber;
+    input.onpaste     = (e) => e.preventDefault();
+    input.oncopy      = (e) => e.preventDefault();
+    input.onkeypress  = validPhoneNumber;
   });
 
-  if (inputEmail) {
-    inputEmail.onpaste = (e) => e.preventDefault();
-    inputEmail.oncopy = (e) => e.preventDefault();
-    inputEmail.oninput = validateEmail;
-  }
+  inputEmails.forEach((input) => {
+    input.onpaste     = (e) => e.preventDefault();
+    input.oncopy      = (e) => e.preventDefault();
+    input.oninput     = validateEmail;
+  });
 };
 
 const main = () => {
