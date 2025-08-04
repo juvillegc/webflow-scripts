2c66d271eb7cea3f64e077f44f68dcacf7a0731d
edhurtad <120751523+edhurtad@users.noreply.github.com>
2025-08-04 16:05:44 -0500

    Update ecosystem.js

--- ecosystem.js
+++ ecosystem.js
@@ -6,24 +6,18 @@ import {
 } from './shared/utils.js';
 
 const main = () => {
-  // 📱 Validación de número celular (ya existente en utils)
-  configurePhoneInput('numero_celular');
 
-  // 🔽 Validación visual del <select>
+  configurePhoneInput('numero_celular');
   configureCustomSelects({
     selectSelector: '.input-b2b-select',
     activeClass: 'selected'
   });
-
-  // ☑️ Al menos un checkbox debe estar seleccionado
   validateCheckboxGroup({
     formSelector: 'form',
     checkboxContainerSelector: '.check-w',
     errorMessage: 'Selecciona al menos un canal antes de enviar.',
     focusClass: 'checkbox-focus'
   });
-
-  // 📧 Solo correos corporativos (no Gmail, Hotmail, etc.)
   validateCorporateEmail({
     inputSelector: '#correo_electronico',
     customMessage: 'Por favor, usa un correo corporativo válido.'
